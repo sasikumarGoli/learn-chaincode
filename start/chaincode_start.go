2136bd440e1a1209218c9a8e8ac9bb5e81a959ec
sasikumarGoli <sasigoli@gmail.com>
2017-04-01 14:43:33 +0530

    addcode

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -150,6 +150,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 	// Handle different functions
 	if function == "read" { //read a variable
+		
+		t := SimpleChaincode{}
 		return t.read(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)
